ba59915ff227f90c63e3b785bd2b3a9a7c80774a
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-21 11:57:08 +0100

    rf(api/counter): slightly reduce calls to upstash -> Bet it'll be minible and useless, but I'm a maniac

--- api/counter.js
+++ api/counter.js
@@ -3,6 +3,8 @@ import { Redis } from 'ioredis';
 const redis = new Redis(`rediss://default:${process.env.REDIS_TOKEN}@${process.env.REDIS_URL}:${process.env.REDIS_PORT}`);
 const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
+let counter = null;
+
 export default async function handler(req, res) {
     const { action, token = null } = req.query;
 
@@ -12,11 +14,15 @@ export default async function handler(req, res) {
         }
 
         const newCount = await redis.incr('counter');
+        counter = newCount;
         return res.status(200).json({ count: newCount });
     }
 
     if (action === 'get') {
+        if (typeof counter === 'number') return res.status(200).json({ count: counter });
+        
         const count = await redis.get('counter') || 0;
+        counter = count;
         return res.status(200).json({ count: parseInt(count, 10) });
     }
 
